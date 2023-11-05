23edc3416e9ed0e48c6e41ae5683a7b05829bfba
Ira-Se <129228693+Ira-Se@users.noreply.github.com>
2023-11-05 11:16:38 +0000

    Deploying to gh-pages from @ Ira-Se/goit-react-hw-05-movies@db3709d0e0d2c6b452b4894cef71a6d5534b1c0a 🚀

---
+++ static/js/282.708b6eb0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[282],{7282:function(e,t,n){n.r(t),n.d(t,{NotFoundPage:function(){return u}});var o=n(1310),r=n(184),u=function(e){e.notFound;return(0,r.jsxs)("div",{children:[(0,r.jsx)(o.DR,{children:"Page not found!"}),(0,r.jsx)(o.xT,{to:"/",children:"Please go to home page"})]})};t.default=u}}]);
+//# sourceMappingURL=282.708b6eb0.chunk.js.map
